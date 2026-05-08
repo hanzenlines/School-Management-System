fb18af88dbb6c5790d7ef311012fd0318580a6ec
Ethan Manto <ethan.manto1@gmail.com>
2026-05-09 01:38:32 +0800

    refactor fix (commited wrong ver) proto

--- Section.java
+++ Section.java
@@ -44,89 +44,89 @@ public class Section {
         return currentCount >= capacity;
     }
 
-    public boolean addStudent() {
-        if (isFull()) {
-            System.out.println("[Section] " + sectionCode + " is already full.");
-            return false;
-        }
-        currentSlots++;
-        System.out.println("[Section] Student added to " + sectionCode
-                + ". Slots: " + currentSlots + "/" + maxSlots);
-        return true;
-        }
-
-    public boolean removeStudent() {
-        if (currentSlots <= 0) {
-            System.out.println("[models.section.Section] No students in " + sectionCode + " to remove.");
-            return false;
-        }
-        currentSlots--;
-        System.out.println("[Section] Student removed from " + sectionCode
-                + ". Slots: " + currentSlots + "/" + maxSlots);
-        return true;
-        }
-
-    public void assignFaculty(Faculty faculty) {
-        this.faculty = faculty;
-        if (this.subject != null) {
-            faculty.addSubject(this.subject.getTitle());
-        }
-
-        System.out.println("[Section] Faculty " + faculty.getEmployeeId()
-                + " assigned to " + sectionCode);
-        }
-
-        public void assignSubject(Subject subject) {
-        this.subject = subject;
-        System.out.println("[Section] Subject " + subject.getSubjectCode()
-                + " - " + subject.getTitle() + " assigned to " + sectionCode);
-        }
-
-        public void assignSchedule(Schedule schedule) {
-        this.schedule = schedule;
-        System.out.println("[Section] Schedule assigned to " + sectionCode);
-        }
-
-        public void displayInfo() {
-        System.out.println("========================================");
-        System.out.println("  SECTION INFO");
-        System.out.println("========================================");
-        System.out.println("  Section ID   : " + sectionId);
-        System.out.println("  Section Code : " + sectionCode);
-
-        if (subject != null) {
-            System.out.println("  Subject      : " + subject.getSubjectCode()
-                    + " - " + subject.getTitle()
-                    + " (" + subject.getUnits() + " units)");
-        } else {
-            System.out.println("  Subject      : Not assigned yet");
-        }
-
-        if (faculty != null) {
-            System.out.println("  Faculty      : " + faculty.getEmployeeId()
-                    + " | Dept: " + faculty.getDepartment());
-        } else {
-            System.out.println("  Faculty      : TBA");
-        }
-
-        System.out.println("  Semester     : " + semester + "  |  S.Y. " + schoolYear);
-        System.out.println("  Slots        : " + currentSlots + " / " + maxSlots
-                + "  (" + getAvailableSlots() + " available)");
-        System.out.println("  Status       : " + (isFull() ? "FULL" : "OPEN"));
-
-        if (!remarks.isEmpty()) {
-            System.out.println("  Remarks      : " + remarks);
-        }
-
-        if (schedule != null) {
-            System.out.println("  --- Schedule ---");
-            schedule.displayInfo();
-        } else {
-            System.out.println("  Schedule     : Not set yet");
-        }
-
-        System.out.println("========================================");
-        }
+//    public boolean addStudent() {
+//        if (isFull()) {
+//            System.out.println("[Section] " + sectionCode + " is already full.");
+//            return false;
+//        }
+//        currentSlots++;
+//        System.out.println("[Section] Student added to " + sectionCode
+//                + ". Slots: " + currentSlots + "/" + maxSlots);
+//        return true;
+//    }
+//
+//    public boolean removeStudent() {
+//        if (currentSlots <= 0) {
+//            System.out.println("[models.section.Section] No students in " + sectionCode + " to remove.");
+//            return false;
+//        }
+//        currentSlots--;
+//        System.out.println("[Section] Student removed from " + sectionCode
+//                + ". Slots: " + currentSlots + "/" + maxSlots);
+//        return true;
+//    }
+//
+//    public void assignFaculty(Faculty faculty) {
+//        this.faculty = faculty;
+//        if (this.subject != null) {
+//            faculty.addSubject(this.subject.getTitle());
+//        }
+//
+//        System.out.println("[Section] Faculty " + faculty.getEmployeeId()
+//                + " assigned to " + sectionCode);
+//    }
+//
+//    public void assignSubject(Subject subject) {
+//        this.subject = subject;
+//        System.out.println("[Section] Subject " + subject.getSubjectCode()
+//                + " - " + subject.getTitle() + " assigned to " + sectionCode);
+//    }
+//
+//    public void assignSchedule(Schedule schedule) {
+//        this.schedule = schedule;
+//        System.out.println("[Section] Schedule assigned to " + sectionCode);
+//    }
+//
+//    public void displayInfo() {
+//        System.out.println("========================================");
+//        System.out.println("  SECTION INFO");
+//        System.out.println("========================================");
+//        System.out.println("  Section ID   : " + sectionId);
+//        System.out.println("  Section Code : " + sectionCode);
+//
+//        if (subject != null) {
+//            System.out.println("  Subject      : " + subject.getSubjectCode()
+//                    + " - " + subject.getTitle()
+//                    + " (" + subject.getUnits() + " units)");
+//        } else {
+//            System.out.println("  Subject      : Not assigned yet");
+//        }
+//
+//        if (faculty != null) {
+//            System.out.println("  Faculty      : " + faculty.getEmployeeId()
+//                    + " | Dept: " + faculty.getDepartment());
+//        } else {
+//            System.out.println("  Faculty      : TBA");
+//        }
+//
+//        System.out.println("  Semester     : " + semester + "  |  S.Y. " + schoolYear);
+//        System.out.println("  Slots        : " + currentSlots + " / " + maxSlots
+//                + "  (" + getAvailableSlots() + " available)");
+//        System.out.println("  Status       : " + (isFull() ? "FULL" : "OPEN"));
+//
+//        if (!remarks.isEmpty()) {
+//            System.out.println("  Remarks      : " + remarks);
+//        }
+//
+//        if (schedule != null) {
+//            System.out.println("  --- Schedule ---");
+//            schedule.displayInfo();
+//        } else {
+//            System.out.println("  Schedule     : Not set yet");
+//        }
+//
+//        System.out.println("========================================");
+//    }
 
     // Getters and Setters
     public String getId() { return id; }
