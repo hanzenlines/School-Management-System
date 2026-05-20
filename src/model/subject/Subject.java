b7101700b400815d823b42d1b2f2db99f11c32de
kmanzl <kmanzalinto@gmail.com>
2026-05-20 19:49:17 +0800

    Added a new class named "Course.java"

--- src/model/subject/Subject.java
+++ src/model/subject/Subject.java
@@ -1,76 +1,78 @@
-package Features;
+package model.subject;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import models.enums.Semester;
+//import models.enums.Semester;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Subject {
-
     private final String id;
     private final String subjectCode;
     private final String subjectName;
     private final int units;
     private final int yearLevel;
-    private final Semester semester;
+    //private final Semester semester;
     private final String course;
     private List<String> prerequisites;
 
     @JsonCreator
     public Subject(
-            @JsonProperty("id")            String id,
-            @JsonProperty("subjectCode")   String subjectCode,
-            @JsonProperty("subjectName")   String subjectName,
-            @JsonProperty("units")         int units,
-            @JsonProperty("yearLevel")     int yearLevel,
-            @JsonProperty("semester")      Semester semester,
-            @JsonProperty("course")        String course,
-            @JsonProperty("prerequisites") List<String> prerequisites
+            @JsonProperty("id")             String id,
+            @JsonProperty("subjectCode")    String subjectCode,
+            @JsonProperty("subjectName")    String subjectName,
+            @JsonProperty("units")          int units,
+            @JsonProperty("yearLevel")      int yearLevel,
+            //@JsonProperty("semester")       Semester semester,
+            @JsonProperty("course")         String course,
+            @JsonProperty("prerequisites")  List<String> prerequisites
     ) {
         this.id = id;
         this.subjectCode = subjectCode;
         this.subjectName = subjectName;
         this.units = units;
         this.yearLevel = yearLevel;
-        this.semester = semester;
+        //this.semester = semester;
         this.course = course;
-        this.prerequisites = prerequisites != null ? new ArrayList<>(prerequisites) : new ArrayList<>();
+        this.prerequisites = prerequisites;
     }
+
 
     public String getId() {
-        return id;
-    }
+         return id; 
+        }
 
     public String getSubjectCode() {
-        return subjectCode;
-    }
+         return subjectCode; 
+        }
 
     public String getSubjectName() {
-        return subjectName;
-    }
+         return subjectName; 
+        }
 
     public int getUnits() {
-        return units;
-    }
+         return units; 
+        }
 
     public int getYearLevel() {
-        return yearLevel;
-    }
+         return yearLevel; 
+        }
+
+    //public Semester getSemester() { return semester; }
 
     public String getCourse() {
-        return course;
-    }
+         return course; 
+        }
 
     public List<String> getPrerequisites() {
-        return Collections.unmodifiableList(prerequisites);
-    }
+         return prerequisites; 
+        }
 
     public void setPrerequisites(List<String> prerequisites) {
-        this.prerequisites = prerequisites != null ? new ArrayList<>(prerequisites) : new ArrayList<>();
+        this.prerequisites = prerequisites != null ? prerequisites : new ArrayList<>();
     }
 }
+ 
